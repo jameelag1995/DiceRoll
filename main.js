48cf5dbff09c68e746f4de0f25870ee76d32159d
jameelag1995 <agbariajameel@gmail.com>
2023-10-05 19:18:01 +0300

    almost ready

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ startButton.addEventListener("click", () => {
         startMsg.innerText += `, Value must be larger than 2`;
     }
 });
+// new game button functionality
 newGameButton.addEventListener("click", () => {
     if (startMsg.innerText.length > 27) {
         const msgArr = startMsg.innerText.split(
@@ -23,7 +24,6 @@ newGameButton.addEventListener("click", () => {
         );
         startMsg.innerText = msgArr.join("");
     }
-    gameOn = true;
     reset();
     startScreen.style.display = "flex";
     instructions.style.visibility = "hidden";
@@ -44,6 +44,8 @@ function reset() {
     player1EndMsg.style.visibility = "hidden";
     player2.classList.remove("player2-win");
     player2EndMsg.style.visibility = "hidden";
+    player1EndMsg.innerText = "";
+    player2EndMsg.innerText = "";
 }
 
 // in-Game elements
@@ -68,10 +70,18 @@ let player2TotalScore = 0;
 let player2CurrentScore = 0;
 let gameOn = true;
 
+// hold button functionality
 holdButton.addEventListener("click", () => {
     if (gameOn === false) {
         return;
     }
+    play();
+    updateScores();
+    checkAndAnnounceWinner();
+});
+
+// function that switches turns
+function play() {
     if (playerTurn === 2) {
         player1.style.background = "#189b9b";
         player2.style.background = "#127a7a";
@@ -81,6 +91,10 @@ holdButton.addEventListener("click", () => {
         player2.style.background = "#189b9b";
         playerTurn = 2;
     }
+}
+
+// function that updates scores and display
+function updateScores() {
     player1TotalScore += player1CurrentScore;
     player2TotalScore += player2CurrentScore;
     player1TotalScoreDisp.innerText = player1TotalScore;
@@ -89,17 +103,27 @@ holdButton.addEventListener("click", () => {
     player2CurrentScore = 0;
     player1CurrentScoreDisp.innerText = player1CurrentScore;
     player2CurrentScoreDisp.innerText = player2CurrentScore;
+}
+
+// function that check and announce the winner
+function checkAndAnnounceWinner() {
     if (player1TotalScore === targetScore || player2TotalScore > targetScore) {
         player1.classList.add("player1-win");
         player1EndMsg.style.visibility = "visible";
+        player1EndMsg.innerText = "You Won!";
+        player2EndMsg.style.visibility = "visible";
+        player2EndMsg.innerText = "Passed the target score";
         gameOn = false;
     }
     if (player2TotalScore === targetScore || player1TotalScore > targetScore) {
         player2.classList.add("player2-win");
+        player2EndMsg.innerText = "You Won!";
         player2EndMsg.style.visibility = "visible";
+        player1EndMsg.style.visibility = "visible";
+        player1EndMsg.innerText = "Passed the target score";
         gameOn = false;
     }
-});
+}
 
 // Dice Functionality
 const dice1 = document.querySelector("#dice-1");
@@ -112,32 +136,46 @@ let diceSum = 0;
 function generateRandomDiceNumber() {
     return Math.ceil(Math.random() * 6);
 }
+
 // when clicking roll dice button generate two random numbers change images and save the sum into diceSum
+
 const rollDiceButton = document.querySelector(".roll-dice-btn");
+// roll button functionality
 rollDiceButton.addEventListener("click", () => {
     if (gameOn === false) {
         return;
     }
+    roll();
+});
+
+function roll() {
+    // generate random number for the first dice
     dice1Value = generateRandomDiceNumber();
+    // change the img of the dice according to the generated dice1Value
     dice1.style.backgroundImage = `url(../Img/dice-${dice1Value}.png)`;
     dice2Value = generateRandomDiceNumber();
     dice2.style.backgroundImage = `url(../Img/dice-${dice2Value}.png)`;
     diceSum = dice1Value + dice2Value;
+    // check if we got 6,6 (sum is 12)
     if (diceSum === 12) {
         if (playerTurn === 1) {
+            // change player1 current score to 0
             player1CurrentScore = 0;
         } else {
             player2CurrentScore = 0;
         }
+        // switch players
         holdButton.click();
         // show popup
     } else {
+        // if sum isn't 12 then add dice sum to player's current score
         if (playerTurn === 1) {
             player1CurrentScore += diceSum;
         } else {
             player2CurrentScore += diceSum;
         }
     }
+    // update current score display
     player1CurrentScoreDisp.innerText = player1CurrentScore;
     player2CurrentScoreDisp.innerText = player2CurrentScore;
-});
+}
